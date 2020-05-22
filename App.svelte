aa5fc41cbdd0330bb3fe231a2b9f0301d48cf4f4
Chris <christophermyoder@gmail.com>
2020-05-22 08:43:31 -0500

    Initial commit

---
+++ App.svelte
@@ -0,0 +1,16 @@
+<script>
+	import Header from './Header.svelte';
+</script>
+
+<style>
+
+  main {
+    font-family: sans-serif;
+    text-align: center;
+    
+  }
+</style>
+
+<main>
+	<Header />
+</main>
---
+++ Header.svelte
@@ -0,0 +1,32 @@
+<script>
+  const headerLinks = ["Self", "Someone Else"];
+</script>
+
+<ul>
+  {#each headerLinks as link}
+    <li class={link}>{link}</li>
+  {/each}
+</ul>
+
+<style>
+  ul {
+    display: grid;
+    grid-template-areas: "self else";
+    grid-template-rows: 50px;
+    grid-template-columns: 1fr 1fr;
+    grid-gap: 1em;
+    align-items: center;
+  }
+  li {
+    list-style: none;
+    border: 1px solid black;
+  }
+
+  .self {
+    grid-area: self;
+  }
+
+  .someoneElse {
+    grid-area: else;
+  }
+</style>
